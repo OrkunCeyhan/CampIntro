dc32dc6dac139e592ecdd91d0cd32306dd6df672
OrkunCeyhan <ceyhanorkun@gmail.com>
2024-12-30 14:24:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Metotlar;
+
+string urunAdi = "Elma";
+double fiyati = 12;
+string aciklama = "elma amasyadan";
+
+Urun urun1 = new Urun();
+urun1.adi = "elma";
+urun1.acıklama = "asd";
+urun1.fiyati = 22;
+
+
+Urun urun2 = new Urun();
+urun2.adi = "ddd";
+urun2.acıklama = "aa";
+urun2.fiyati = 222;
+
+Urun[] urunler = new Urun[] {urun1, urun2};
+foreach (var item in urunler)
+{
+    Console.WriteLine("-------------");
+    Console.WriteLine(item.adi);
+    Console.WriteLine(item.acıklama);
+    Console.WriteLine(item.fiyati);
+
+}
+
+Console.WriteLine(  "-------metotlar");
+
+SepetManager sepetManager = new SepetManager();
+sepetManager.Ekle(urun1);
+sepetManager.Ekle(urun2);
---
+++ Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public  class Urun
+    {
+        public int Id { get; set; }
+        public string adi { get; set; }
+        public double fiyati { get; set; }
+        public string acıklama { get; set; }
+
+    }
+}
